65dfd580ca3f5b1717de5cd24eca0385f470061b
Aidan O'Connor <aocon057@uottawa.ca>
2019-05-22 17:49:29 -0400

    Update Credentials.php

--- Mitchell/RestaurantWebsite/Model/Credentials.php
+++ Mitchell/RestaurantWebsite/Model/Credentials.php
@@ -1,10 +1,1 @@
-<?php
-
-//Login data for the database. Use this file in all Models
-$host = "jdbc:postgresql://www.eecs.uottawa.ca:15432/mchat022?currentSchema=Project";
-$port = '15432';
-$user = "mchat022";
-$passwd = "M2rnay6gz9";
-$database = "Project";
-$conn_string="host=www.eecs.uottawa.ca port=15432 dbname=Project user=mchat022 password=M2rnay6gz9";
-?>
+//Private info
